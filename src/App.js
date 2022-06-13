c299a297a52253adc1d45a01399796c895343fc9
Akib Zawayed <56231072+akib0079@users.noreply.github.com>
2022-06-13 12:07:59 +0600

    hero section updated

--- src/App.js
+++ src/App.js
@@ -5,11 +5,55 @@ import 'animate.css';
 import Footer from './Statics/Footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Loading from './Pages/Loading/loading';
+import { useEffect, useState } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+
 
 function App() {
+
+  const [siteLoading, setSiteLoading] = useState(false);
+
+  useEffect(() => {
+    setSiteLoading(true);
+
+    setTimeout(() => {
+      setSiteLoading(false)
+    }, 2000)
+  }, []);
+
+
+  document.addEventListener('mousemove', e => {
+    const cursor = document.getElementById('cursor');
+    cursor.setAttribute("style", "top: " + (e.pageY - 15) + 'px; left: ' + (e.pageX - 15) + 'px');
+  })
+
+  document.addEventListener('click', () => {
+    const cursor = document.getElementById('cursor');
+
+    cursor.classList.add('expend');
+    setTimeout(() => {
+      cursor.classList.remove('expend');
+    }, 500)
+  });
+
+
+  AOS.init();
+
+
+  if (siteLoading) return <Loading></Loading>
+
   return (
-    <div className="App">
+    <div className="App animate__animated animate__fadeIn">
       <Header></Header>
+
+      {/* custom cursor */}
+      <div id='cursor' className="cursor"></div>
+
+
 
       <Routes>
         <Route path="/" element={<Home />} />
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './Home.css';
-import mainImg from '../../images/main image.png'
+import mainImg from '../../images/main image.webp'
+import reactIcn from '../../images/react-icn.svg';
+import nodeIcn from '../../images/node-icn.svg';
+import jsIcn from '../../images/js-icn.svg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <section className="hero container mx-auto p-5 lg:p-0">
-            <div className="sectionInner grid lg:grid-cols-2 items-center">
+            <div className="sectionInner grid lg:grid-cols-2 items-center gap-4">
                 <div className="info">
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-secondary">Get Started</button>
+                    <p className='badge'>FullStack Web developer : Akib0079</p>
+                    <h1 className="md:text-5xl">It's not a bug. It's an undocumented feature
+                        <i class='bx bx-code-curly codeIcn' ></i></h1>
+                    <p className="py-6">Hello, this is Akib Zawayed. A Designer, A Developer, and A Wordpress-Expert. Trying to make the Web better and more innovative in my own way.</p>
+                    <Link to={'/about'}><button className="btn btn-secondary">Get Started</button></Link>
                 </div>
-                <div className="img">
+                <div className="imgSec" data-aos="fade-left" >
                     <img className='w-4/5 mx-auto' src={mainImg} alt="" />
+                    <img className='icnReact' src={reactIcn} alt="" />
+                    <img className='icnNode' src={nodeIcn} alt="" />
+                    <img className='icnJs' src={jsIcn} alt="" />
                 </div>
             </div>
         </section>
