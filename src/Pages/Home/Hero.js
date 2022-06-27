78281307710960a68d508444ffdc0f2883f5581a
Akib Zawayed <56231072+akib0079@users.noreply.github.com>
2022-06-27 16:29:46 +0600

    svg image path updated for better User experience

--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -28,19 +28,23 @@ const Hero = () => {
         config: config.molasses,
     });
 
+
+
     return (
         <section className="HeroSec" id='hero'>
             <div className="hero container mx-auto p-5 lg:p-0">
-                <div className="sectionInner grid md:grid-cols-2 items-center gap-4">
+                <div className="sectionInner grid lg:grid-cols-2 items-center gap-4">
                     <div className="info">
-                        <p className='badge'>FullStack Web developer : Akib0079</p>
-                        <h1 className="md:text-5xl">It's not a bug. It's an undocumented feature
+                        <p className='badge' data-aos="fade-up" data-aos-delay="500" data-aos-duration="700">FullStack Web developer : Akib0079</p>
+                        <h1 className="md:text-5xl" data-aos="fade-up" data-aos-delay="500" data-aos-duration="700">It's not a bug. It's an undocumented feature
                             <i className='bx bx-code-curly codeIcn' ></i></h1>
-                        <p className="py-6">Hello, this is Akib Zawayed. A Designer, A Developer, and A Wordpress-Expert. Trying to make the Web better and more innovative in my own way.
+                        <p className="py-6" data-aos="fade-up" data-aos-delay="600" data-aos-duration="700">Hello, this is Akib Zawayed. A Designer, A Developer, and A Wordpress-Expert. Trying to make the Web better and more innovative in my own way.
                         </p>
-                        <button className="mainBtn mt-2 mb-9">Lets Chat! <i className='bx bxs-conversation' ></i></button>
+                        <a href="mailto:akibzawayed0079@gmail.com">
+                            <button className="mainBtn mt-2 mb-9" data-aos="fade-up" data-aos-delay="700" data-aos-duration="700">Lets Chat! <i className='bx bxs-conversation' ></i></button>
+                        </a>
                         {/* counters */}
-                        <div className="flex p-6 counters justify-center lg:justify-start mt-10 shadow-2xl items-center flex-col md:flex-row" data-aos="flip-up" data-aos-delay="300" data-aos-duration="1200">
+                        <div className="p-6 counters justify-center lg:justify-start mt-10 shadow-2xl items-center flex-col md:flex-row hidden lg:flex " data-aos="flip-up" data-aos-delay="300" data-aos-duration="1200">
                             <div className="counter flex items-center">
                                 <h3 className='flex'>0<animated.div>{number.to(n => n.toFixed(0))}</animated.div>+</h3>
                                 <p>Years of<br></br> Experience.</p>
@@ -53,11 +57,11 @@ const Hero = () => {
                         </div>
 
                     </div>
-                    <div className="imgSec" data-aos="fade-left" data-aos-delay="300">
+                    <div className="imgSec" data-aos="fade-up" data-aos-delay="300">
                         <img className='w-4/5 mx-auto' src={mainImg} alt="" />
-                        <img className='icnReact' src={reactIcn} alt="" />
-                        <img className='icnNode' src={nodeIcn} alt="" />
-                        <img className='icnJs' src={jsIcn} alt="" />
+                        <img className='icnReact heroIcns' src={reactIcn} alt="" />
+                        <img className='icnNode heroIcns' src={nodeIcn} alt="" />
+                        <img className='icnJs heroIcns' src={jsIcn} alt="" />
                     </div>
                 </div>
             </div>
